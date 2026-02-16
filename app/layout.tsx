a4ab2e55c24369654eab50d99f2025d73a694913
KarimKribi02 <abdelkarimkribi@gmail.com>
2026-02-16 13:50:03 +0000

    veficiation google

--- app/layout.tsx
+++ app/layout.tsx
@@ -67,6 +67,9 @@ export const metadata: Metadata = {
     description: 'Digital Marketing & Web Development at its finest.',
     images: ['/og-image.jpg'],
   },
+  verification: {
+    google: 'I42sCPR4NxgnW8nguoJ_TmUS8H0_B5HeX4hUEs0o6G8', // Had l-koud ni-chan
+  },
 };
 
 export default function RootLayout({
